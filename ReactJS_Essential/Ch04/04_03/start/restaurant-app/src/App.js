db865be0779969adec65427fb5e8567637d81dec
Juan Carlo A. Banayo <johncban@hotmail.com>
2024-01-06 00:51:29 -0500

    Finish E04_04

---
+++ ReactJS_Essential/Ch04/04_03/start/restaurant-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const [firstCity, second] = ["Tokyo", "Tahoe City", "Bend"];
+
+//console.log(cities[0]);
+
+console.log(firstCity);
+console.log(second);
+
+function App({ library }) {
+  return (
+    <div className="App">
+      <h1>Hello from {library}</h1>
+    </div>
+  );
+}
+
+export default App;
